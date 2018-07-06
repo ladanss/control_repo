8029ef30df91a9ca627771f783255d4b1dd0b1fd
ladanss <40870790+ladanss@users.noreply.github.com>
2018-07-05 22:50:57 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,8 +8,8 @@ node default {
 node 'master.puppet.vm' {
   include role::master_server
 }
-node /^web/ {
-  include role::web_server
+node /^web/ { 
+  include role::app_server
 }
 node /^db/ {
   include role::db_server
